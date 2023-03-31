10db5e82f0381941382e3a1c392ef28796ec6303
Randy Hunter <randyh252@gmail.com>
2023-03-31 16:50:31 -0400

    updated pages

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
+// import components
+import LatestProducts from '../components/LatestProducts.js';
+import Hero from '../components/Hero.js';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section>
+      <Hero />
+      <LatestProducts />
+    </section>
+  );
 };
 
 export default Home;
